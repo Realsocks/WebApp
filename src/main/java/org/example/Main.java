56ba15241cacc31a14447335aa5cef08404a92a1
Realsocks <kzeltov110@gmail.com>
2022-10-22 11:52:56 +0400

    commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -2,6 +2,9 @@ package org.example;
 
 public class Main {
     public static void main(String[] args) {
-        
+        //testing git
+        for(int i=0;i<10;i++){
+            System.out.println("Git working");
+        }
     }
 }
